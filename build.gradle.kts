c07586743ac48b45785aa81f1476eb175e9e1410
tmdghlrla <tmdghlrla@google.com>
2024-02-26 10:04:06 +0900

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
+    val agp_version by extra("8.1.3")
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.1.3" apply false
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
